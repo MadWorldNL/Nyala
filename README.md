7347b36a7599fa45901291247dcce8deab00678f
Oscar Veldman <oveldman@gmail.com>
2024-12-22 10:51:54 +0100

    Update ReadMe

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # Nyala
+*TODO: Write description*
+
+> [!WARNING]
+> Currently, Nyala is in the setup phase and is not yet usable.
+
+## Key Features
+*TODO: Write key features*
+
+## Getting Started
+### Prerequisites
+Download the following software
+* Visual Studio Code
+* Docker 
+
+### Installation
+Installation guide for your development environment
+#### Option 1: From Docker
+* Clone this repository
+* Run this command in the source folder:
+```shell
+docker compose up
+```
+
+#### Option 2: From your editor
+*TODO: Install native way*
+
+## Documentation
+*TODO: Link to documentation*
 
 ## Acknowledgements
 I would like to thank the following sources and individuals for their templates, which I used in this application:
--- sources/Clients.EndUserViewer/clients_end_user_viewer/lib/functions/https/http_request_sender.dart
+++ sources/Clients.EndUserViewer/clients_end_user_viewer/lib/functions/https/http_request_sender.dart
@@ -5,6 +5,30 @@ class HttpRequestSender {
   Future<String> sendRequest(HttpMethodes methode, String url) async {
     var response = await http.get(Uri.parse(url));
 
+    switch (methode) {
+      case HttpMethodes.get:
+        response = await http.get(Uri.parse(url));
+        break;
+      case HttpMethodes.post:
+        response = await http.post(Uri.parse(url));
+        break;
+      case HttpMethodes.put:
+        response = await http.put(Uri.parse(url));
+        break;
+      case HttpMethodes.delete:
+        response = await http.delete(Uri.parse(url));
+        break;
+      case HttpMethodes.patch:
+        response = await http.patch(Uri.parse(url));
+        break;
+      case HttpMethodes.head:
+        response = await http.head(Uri.parse(url));
+        break;
+      case HttpMethodes.options:
+      case HttpMethodes.trace:
+        break;
+    }
+
     if (response.statusCode == 200)
     {
       return response.body;
